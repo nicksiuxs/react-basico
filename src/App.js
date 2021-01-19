fb5a6a2290500ea07ff8bfcacf1545efc64be4cb
Nicolas <nicksiuxs@gmail.com>
2021-01-18 20:16:03 -0500

    ADD: props were added This is the hello world of the props in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
 
 
 function App() {
+
+  // Obtener la fecha
+  const fecha = new Date().getFullYear();
   return (
     <Fragment>
-      <Header/>
-      <Footer/>
+      <Header titulo="Nueva tienda" />
+      <Footer fecha={fecha}/>
     </Fragment>
   );
 }
