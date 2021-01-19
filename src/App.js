9d85b8a4223b8521162364e015de50d4a6cbffd7
Nicolas <nicksiuxs@gmail.com>
2021-01-18 20:05:25 -0500

    ADD:  footer component was added The footer was added in the project and the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import React, {Fragment} from 'react'
 import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
 
 function App() {
   return (
-    <div className="App">
+    <Fragment>
       <Header/>
-    </div>
+      <Footer/>
+    </Fragment>
   );
 }
 
