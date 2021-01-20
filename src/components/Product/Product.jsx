a19462f34f3d7fdf027ceda53ad36119e14edf5e
Nicolas <nicksiuxs@gmail.com>
2021-01-19 19:50:46 -0500

    ADD: button add product to shopping car

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,18 @@
 const Producto = ({product}) => {
     const {id, nombre, precio} = product;
+
+    // Agregar producto al carrito
+    const seleccionarProducto = (id)=>{
+        console.log("comprando... ", id)
+    }
     return ( 
         <div>
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button 
+                type="button"
+                onClick={()=> seleccionarProducto(id)}
+            >Comprar</button>
         </div>
     );
 }
